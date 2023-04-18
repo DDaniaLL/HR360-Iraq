93d68132a9c8d497b25f7acdf11beb653a54a8ce
DDaniall <dany.janboura@gmail.com>
2023-04-19 01:30:29 +0300

    copy the content of public, resources and routes

---
+++ resources/lang/en/createOvertime.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'newOvertime' => 'New Overtime Request' ,
+    'newOvertimedetails' => 'New Overtime Request details' ,
+    'overtimeType' => 'Overtime Type' ,
+    'Date' => 'Date',
+    'starthour' => 'Start Hour',
+    'endhour' => 'End Hour' ,
+'ServiceContractOvertime' => 'Service Contract Overtime',
+'workday' => 'Workday',
+'Week-end' => 'Week-end',
+'Holiday' => 'Holiday',
+'suntothu' => '(Sun to Sat)',
+'friorsat' => '(Friday)',
+'message' => 'Choose Overtime Type first',
+    'reason' => 'Reason',
+    'comment' => 'Comment',
+    'hours' => 'Hours'  ,
+    'between' => 'between' ,
+    'and' => 'and' ,
+    'attachment' => 'Attachment' ,
+    'chooseFile' => 'Choose File' ,
+    'noFileChosen' => 'no file chosen',
+    'totalNumberOfDays' => 'Total Number of Days' ,
+    'enterStartDateAndEndDate' => 'Enter Start Date and End Date' ,
+    'submit' => 'Submit' ,
+    'cancel' => 'Cancel',
+    'extra' =>'Extra Approver:',
+    'note' =>'Overtime Request will be approved only if 48 work hours are completed in the week',
+
+];
